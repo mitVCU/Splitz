e4b551575b8784373e61a3e77d31a3d86c60b581
Mit Amin <mit.na14@gmail.com>
2020-12-28 20:38:56 -0500

    Initial Commit

---
+++ Splitz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Splitz
+//
+//  Created by Mit Amin on 12/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
