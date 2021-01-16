7576e680ad4954fca0ca5c2abd49051ba1543934
Mit Amin <mit.na14@gmail.com>
2021-01-16 17:42:55 -0500

    added string localizaiton and set up the basics of the Bill Scanner screen

--- Splitz/ViewController.swift
+++ Splitz/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var billScanningStatusLabel: UILabel!
+    @IBOutlet weak var manualAmountButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func configureManualAmountEntryButton() {
+        manualAmountButton.titleLabel?.text = "Enter Amount Manually"
+    }
 
 }
 
